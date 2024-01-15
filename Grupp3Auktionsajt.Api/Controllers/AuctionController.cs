e38cd304d5bc92ab73d1e12d0a0f4dd50f12cc13
Obi-Wan-More-Rep <kevin.branger@live.com>
2024-01-15 13:46:50 +0100

    började med en controller

---
+++ Grupp3Auktionsajt.Api/Controllers/AuctionController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Grupp3Auktionsajt.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grupp3Auktionsajt.Api.Controllers
+{
+    [Route("api/auction")]
+    [ApiController]
+    public class AuctionController : ControllerBase
+    {
+        private readonly IAuctionService _service;
+        private readonly IMapper _mapper;
+
+        public AuctionController(IAuctionService service)
+        {
+            _service = service;
+        }
+    }
+}
