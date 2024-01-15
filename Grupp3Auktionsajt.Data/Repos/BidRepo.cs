164720452a40312764348245490bc97bf6b412e3
Kristina Sviridova <sviridova.kristina@hotmail.com>
2024-01-15 11:33:18 +0100

    Delete auction and delete bid updete

--- Grupp3Auktionsajt.Data/Repos/BidRepo.cs
+++ Grupp3Auktionsajt.Data/Repos/BidRepo.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,22 @@ namespace Grupp3Auktionsajt.Data.Repos
 {
     internal class BidRepo
     {
+        private readonly DBContext _context;
+
+        public BidRepo(DBContext context)
+        {
+            _context = context;
+        }
+
+        public static void DeleteBid(int bidID)
+        {
+            using (var db = _context.GetConnection())
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("@BidID", bidID);
+
+                db.Execute("DeleteBid", parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
     }
 }
