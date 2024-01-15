5d7300bca18840292a0b60baa2c1c2794ecf9dcc
Obi-Wan-More-Rep <kevin.branger@live.com>
2024-01-15 13:26:54 +0100

    Lat till ".Data" references i  Core. Försvann av någon anledning

--- Grupp3Auktionsajt.Core/Services/AuctionService.cs
+++ Grupp3Auktionsajt.Core/Services/AuctionService.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grupp3Auktionsajt.Core.Interfaces;
 using Grupp3Auktionsajt.Data.Interfaces;
 
 
 namespace Grupp3Auktionsajt.Core.Services
 {
-    public class AuctionService
+    public class AuctionService : IAuctionService
     {
         private readonly IAuctionRepo _repo;
+
+    }
