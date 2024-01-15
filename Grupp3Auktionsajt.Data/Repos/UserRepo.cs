b71069abb95a7fb11e80712fac095a9028a1e86a
Mattias89 <mattiassaeed89@gmail.com>
2024-01-15 15:53:03 +0100

    User Login Model

--- Grupp3Auktionsajt.Data/Repos/UserRepo.cs
+++ Grupp3Auktionsajt.Data/Repos/UserRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,34 @@ namespace Grupp3Auktionsajt.Data.Repos
 {
     internal class UserRepo
     {
+        public class UserLoginModel
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
 
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(UserLoginModel user)
+        {
+            // Här ska du lägga till koden för att ansluta till databasen
+            // och anropa din Stored Procedure med användaruppgifterna.
+
+            // Låt oss anta att du har en metod som heter `AuthenticateUser` som gör detta och returnerar UserId
+            var userId = AuthenticateUser(user.Username, user.Password);
+
+            if (userId != null)
+            {
+                // Användaren är autentiserad
+                return Ok(userId);
+            }
+            else
+            {
+                // Autentisering misslyckades
+                return UnauthorizedResult();
+            }
+        }
+
+        
     }
 }
