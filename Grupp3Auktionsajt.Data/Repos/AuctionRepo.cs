056c6c88f8cea19e50453296bebcf8672a7cefea
Kristina Sviridova <sviridova.kristina@hotmail.com>
2024-01-15 10:20:55 +0100

    Created Repos class

---
+++ Grupp3Auktionsajt.Data/Repos/AuctionRepo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp3Auktionsajt.Data.Repos
+{
+    public class AuctionRepo
+    {
+
+        private readonly DBContext _context;
+
+        public AuctionRepo(DBContext context)
+        {
+            _context = context;
+        }
+
+        public static void DeleteAuction(int auctionID)
+        {
+            using (IDbConnection db = new SqlConnection(_connString))
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("@AuctionID", auctionID);
+
+                db.Execute("DeleteAuction", parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+    }
+}
---
+++ Grupp3Auktionsajt.Data/Repos/UserRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp3Auktionsajt.Data.Repos
+{
+    internal class UserRepo
+    {
+    }
+}
