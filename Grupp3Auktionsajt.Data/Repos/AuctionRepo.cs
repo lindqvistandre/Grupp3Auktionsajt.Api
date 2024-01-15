686b7d93abff179c55eb31316da7fa968ac4d836
Obi-Wan-More-Rep <kevin.branger@live.com>
2024-01-15 15:07:51 +0100

    GJorde repository metoden för CreateAuction

--- Grupp3Auktionsajt.Data/Repos/AuctionRepo.cs
+++ Grupp3Auktionsajt.Data/Repos/AuctionRepo.cs
@@ -1,5 +1,6 @@
 ﻿using Dapper;
 using Grupp3Auktionsajt.Data.Interfaces;
+using Grupp3Auktionsajt.Domain.Models.DTO;
 using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
@@ -30,6 +31,20 @@ namespace Grupp3Auktionsajt.Data.Repos
                 parameters.Add("@AuctionID", auctionID);
 
                 db.Execute("DeleteAuction", parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public void CreateAuction (CreateAuctionDTO auctionDTO, int UserId)
+        {
+            using (var db = _context.GetConnection())
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("@Title", auctionDTO.Title);
+                parameters.Add("@Description", auctionDTO.Description);
+                parameters.Add("@Price", auctionDTO.Price);
+                parameters.Add("@Days", auctionDTO.Days);
+
+                db.Execute("sp_CreateAuction", parameters, commandType: CommandType.StoredProcedure);
             }
         }
     }
---
+++ Grupp3Auktionsajt.Domain/Models/DTO/CreateAuctionDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp3Auktionsajt.Domain.Models.DTO
+{
+    public class CreateAuctionDTO
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Days { get; set; }
+    }
+}
