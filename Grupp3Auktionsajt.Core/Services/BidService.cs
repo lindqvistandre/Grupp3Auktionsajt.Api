1eb5ca631089d0708ba0cd201dd6793c33220c79
Kristina Sviridova <sviridova.kristina@hotmail.com>
2024-01-15 15:47:03 +0100

    Created BidService class

---
+++ Grupp3Auktionsajt.Core/Services/BidService.cs
@@ -0,0 +1,25 @@
+﻿using Grupp3Auktionsajt.Data.Interfaces;
+using Grupp3Auktionsajt.Domain.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp3Auktionsajt.Core.Services
+{
+    public class BidService
+    {
+        private readonly IBidRepo _repo;
+
+        public BidService(IBidRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public void DeleteBid(int bidID)
+        {
+            _repo.DeleteBid(bidID);
+        }
+    }
+}
