7eb4c8df6fe3341e33555daad4063a2d87829f60
Obi-Wan-More-Rep <kevin.branger@live.com>
2024-01-17 10:41:42 +0100

    Sorterade "Skapa alla stored procedures" efter tabellnamn

--- Skapa alla stored procedures.sql
+++ Skapa alla stored procedures.sql
@@ -1,3 +1,33 @@
+------------------------------- Users table
+CREATE PROCEDURE sp_UserLogin
+    @Username NVARCHAR(50),
+    @Password NVARCHAR(50)
+AS
+BEGIN
+    SELECT UserId
+    FROM Users
+    WHERE Username = @Username AND Password = @Password
+END
+GO
+
+-- Föregående kod om det finns...
+GO
+
+CREATE PROCEDURE UserLogin
+    @Username NVARCHAR(50),
+    @Password NVARCHAR(50)
+AS
+BEGIN
+    SELECT UserId
+    FROM Users
+    WHERE Username = @Username AND Password = @Password
+END
+GO
+
+
+
+--------------------------------Bid table
+
 -- Det går även att ångra dvs ta bort ett bud om auktionen inte är avslutad.
 
 Create procedure sp_DeleteBid(@DeleteByID int)
@@ -7,6 +37,35 @@ Delete
 From Bids
 Where BidId = @DeleteByID
 End;
+
+
+-- stored procedure för lista bid -- 
+CREATE PROCEDURE sp_GetBidsForAuction
+    @AuctionId INT
+AS
+BEGIN
+    SELECT b.BidId, b.UserId, b.BidPrice, b.BidTimeStamp 
+    FROM Bids b 
+    WHERE b.AuctionId = @AuctionId
+    ORDER BY b.BidPrice DESC;
+END;
+
+
+-- stored procedure för create bid --
+
+CREATE PROCEDURE CreateBid
+    @AuctionId INT,
+    @UserId INT,
+    @BidPrice DECIMAL(18, 2)
+AS
+BEGIN
+    INSERT INTO Bids (AuctionId, UserId, BidPrice)
+    VALUES (@AuctionId, @UserId, @BidPrice);
+END;    
+
+
+-------------------------------Auction table
+
 
 --Finns det inga bud på en auktion skall den kunna tas bort. Det skall även gå att
 Create procedure sp_DeleteAuction(@DeleteByID int)
@@ -33,56 +92,6 @@ begin
 	values (@Title, @Description, @Price, @StartDate, @EndDate, @CreatorUserId);
 end;
 
-
--- stored procedure för lista bid -- 
-CREATE PROCEDURE sp_GetBidsForAuction
-    @AuctionId INT
-AS
-BEGIN
-    SELECT b.BidId, b.UserId, b.BidPrice, b.BidTimeStamp 
-    FROM Bids b 
-    WHERE b.AuctionId = @AuctionId
-    ORDER BY b.BidPrice DESC;
-END;
-
--- stored procedure för create bid --
-
-CREATE PROCEDURE CreateBid
-    @AuctionId INT,
-    @UserId INT,
-    @BidPrice DECIMAL(18, 2)
-AS
-BEGIN
-    INSERT INTO Bids (AuctionId, UserId, BidPrice)
-    VALUES (@AuctionId, @UserId, @BidPrice);
-END;    
-
-
-CREATE PROCEDURE sp_UserLogin
-    @Username NVARCHAR(50),
-    @Password NVARCHAR(50)
-AS
-BEGIN
-    SELECT UserId
-    FROM Users
-    WHERE Username = @Username AND Password = @Password
-END
-GO
-
--- Föregående kod om det finns...
-GO
-
-CREATE PROCEDURE UserLogin
-    @Username NVARCHAR(50),
-    @Password NVARCHAR(50)
-AS
-BEGIN
-    SELECT UserId
-    FROM Users
-    WHERE Username = @Username AND Password = @Password
-END
-GO
-
 -- Eventuell efterföljande kod...
 
 CREATE PROCEDURE UpdateAuctionPrice
@@ -95,3 +104,9 @@ BEGIN
     WHERE AuctionId = @AuctionId
 END
 GO
+
+
+
+
+
+
